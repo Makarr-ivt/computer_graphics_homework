944d1635d2efd5608010645c477c8f5747351f7c
Makarr <maanromanov@edu.hse.ru>
2024-10-09 19:21:19 +0300

    Comments added

--- video_compression.py
+++ video_compression.py
@@ -3,12 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def get_average_color(frame_data):
+    """Вычисляет средний цвет из кадрового изображения."""
     array = np.frombuffer(frame_data, np.uint8)
+    # array -- одномерный массив из всех пикселей кадра
     average_color = np.mean(array)  # среднее значение цвета по кадру
     return average_color
 
 def analyze_video_compression(original_video, compressed_video, plot_path):
+    """Сравнивает оригинальное и сжатое видео по кадрам, строит график разницы"""
     print("Анализ потерь при сжатии видео (может занять некоторое время)")
+    # Получаем информацию о видео
     probe_original = ffmpeg.probe(original_video)
     num_frames = int(probe_original['streams'][0]['nb_frames'])
     # num_frames_original == num_frames_compressed
@@ -23,6 +27,7 @@ def analyze_video_compression(original_video, compressed_video, plot_path):
     max_difference: int = 0
     max_difference_time: float = 0.0
     for i in range(num_frames):
+        # Извлечение кадра из оригинального видео
         original_frame = (
             ffmpeg
             .input(original_video, ss=i/fps, t=1/fps)
@@ -30,6 +35,7 @@ def analyze_video_compression(original_video, compressed_video, plot_path):
             .run(capture_stdout=True, capture_stderr=True)
         )
         
+        # то же из сжатого видео
         compressed_frame = (
             ffmpeg
             .input(compressed_video, ss=i/fps, t=1/fps)
@@ -37,38 +43,46 @@ def analyze_video_compression(original_video, compressed_video, plot_path):
             .run(capture_stdout=True, capture_stderr=True)
         )
         
+        # Получаем средние цвета
         avg_color_original = get_average_color(original_frame[0])
         avg_color_compressed = get_average_color(compressed_frame[0])
         
+        # Вычисляем разницу (потерю качества)
         difference = abs(avg_color_compressed - avg_color_original)
         differences.append(difference)
+        # поиск кадра с максимальными потерями
         if difference > max_difference:
             max_difference = difference
             max_difference_time = i / fps
 
+    # Построение графика потерь
     plt.plot(range(num_frames), differences)
     plt.xlabel("Кадр")
     plt.ylabel("Разница среднего цвета")
     plt.title("Разница среднем цвете между оригинальным и сжатым видео")
     plt.grid()
+    # точка для выделения кадра с наибольшей потерей качества
     plt.scatter(max_difference_time * fps, max_difference, color='red', label=f'Макс. разница на {max_difference_time} секунде')
     plt.legend()
     plt.savefig(plot_path)
     print("Построен график")
 
-
-
 def compress_video(filename: str, output_filename: str):
+    """Из видеофайла filename делает сжатое видео output_filename с пониженным битрейтом до 2 Мбит/сек"""
     print("Видео Сжимается")
     input_stream = ffmpeg.input(filename)
+    # Применение видеофильтра для изменения контрастности
     video_stream = input_stream.filter('eq', contrast=5.0)
+    # фильтр накладывается отдельно на video_stream, а не на input_stream, 
+    # Потому что иначе звуковая дорожка пропадает
     (
+        # Вывод видео с новым битрейтом и сохранением оригинальной аудиодорожки
         ffmpeg.output(
             video_stream, input_stream.audio,
             output_filename,
             video_bitrate='2M',
-            vcodec='libx264',
-            acodec='aac',
+            vcodec='libx264', # дефолтный кодек в ffmpeg, почти как h.264
+            acodec='aac', # тоже дефолтный кодек для аудио
         )
         .run()
     )
