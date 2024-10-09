72ab9315e24f2f4c3cfa274ea24c1be2316bf9ec
Makarr <maanromanov@edu.hse.ru>
2024-10-09 19:09:58 +0300

    main.py project template

---
+++ main.py
@@ -0,0 +1,13 @@
+def compress_video():
+    pass
+
+def analyze_video_compression():
+    pass
+
+def main():
+    compress_video()
+    analyze_video_compression()
+    exit()
+
+if __name__ == "__main__":
+    main()
